1f6dfd636bafad53a096dfb32c67f0af55de2933
cKatand <ckatand@gmail.com>
2024-07-11 14:30:47 -0400

    ok

--- repaso/01/script.js
+++ repaso/01/script.js
@@ -2,5 +2,5 @@ function contador(elemento) {
     let contador = parseInt(elemento.getAttribute('data-contador') || '0');
     contador ++;
     elemento.setAttribute('data-contador', contador);
-    elemento.textContent = 'Clics' + contador;
+    elemento.textContent = 'Clic ' + contador;
 }
